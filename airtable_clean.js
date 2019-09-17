967f9d24a655e89dba3cbaa843efedf5d1f0f4ee
ckccyu <ckccyu@gmail.com>
2019-09-17 22:29:31 +0800

    [Update] clean function

--- airtable_clean.js
+++ airtable_clean.js
@@ -1,5 +1,34 @@
 const { retrieve } = require('./airtable_retrieve.js');
+const { base } = require('./airtable_connection.js');
 
-retrieve('Table 9', 'Name', 'test').then((retrievedEntries) => {
-  console.log(retrievedEntries);
-});
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+function cleanEntry(id) {
+  return new Promise((resolve) => {
+    base(table).destroy([id], (err, deletedRecords) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      resolve();
+    });
+  });
+}
+
+const table = 'Table 9';
+async function clean() {
+  let count = 0;
+  let retrievedEntries = await retrieve(table, 'Name', 'test');
+
+  for (i = 0; i < retrievedEntries.length; i++) {
+    await sleep(500);
+    await cleanEntry(retrievedEntries[i].id);
+    count++;
+  }
+
+  console.log('Deleted', count, 'records.');
+};
+
+clean();

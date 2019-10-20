57c8b212290624b44086c947e5a922b6a0ef96c1
ckccyu <ckccyu@gmail.com>
2019-10-20 21:30:12 +0800

    [Fix] adjust for lambda file structure

--- line/deploy.ps1
+++ line/deploy.ps1
@@ -1,7 +1,7 @@
 Copy-Item "C:\Users\chihy\OneDrive\Ongoing - OneDrive\app-AgingPlus\line\lambda.py" `
 	-Destination "C:\Users\chihy\OneDrive\Ongoing - OneDrive\app-AgingPlus\line\build"
 
-Compress-Archive -path "C:\Users\chihy\OneDrive\Ongoing - OneDrive\app-AgingPlus\line\build" `
+Compress-Archive -Path "C:\Users\chihy\OneDrive\Ongoing - OneDrive\app-AgingPlus\line\build\*" `
 	-destinationpath "C:\Users\chihy\OneDrive\Ongoing - OneDrive\app-AgingPlus\line\Line.zip" `
 	-compressionlevel optimal -Force
 

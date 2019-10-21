b4df0e9d4a90355ef24b44f535df542e5857a151
ckccyu <ckccyu@gmail.com>
2019-10-21 21:11:25 +0800

    [Add] create airtable record for line member

--- lambda_airtable/add-new-member.js
+++ lambda_airtable/add-new-member.js
@@ -1,3 +1,20 @@
 const Airtable = require("airtable");
 
-exports.base = new Airtable({ apiKey: process.env.AIRTABLE_APIKEY }).base(process.env.BASE_ID);
+const base = new Airtable({ apiKey: process.env.AIRTABLE_APIKEY }).base(process.env.BASE_ID);
+
+base('LINE-MEMBER').create([
+  {
+    "fields": {
+      "LineUserId": "hello",
+      "LineDisplayName": "CY"
+    }
+  }
+], function(err, records) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  records.forEach((record) => {
+    console.log('Added ' + record.fields.LineDisplayName + ' to Line member record.');
+  });
+});

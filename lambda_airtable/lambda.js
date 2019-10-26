f0b39bcc00d93c9599e32fd858b14b51a48cdd49
ckccyu <ckccyu@gmail.com>
2019-10-26 23:49:38 +0800

    [Add] filter target for reminder message

--- lambda_airtable/lambda.js
+++ lambda_airtable/lambda.js
@@ -1,8 +1,11 @@
 const Airtable = require("airtable");
+const moment = require('moment');
+const { sleep } = require('./util');
 
 const base = new Airtable({ apiKey: process.env.AIRTABLE_APIKEY }).base(process.env.BASE_ID);
 const AIR_EVENT_TYPES = {
-  FOLLOW: 'follow'
+  FOLLOW: 'follow',
+  REMINDER: 'reminder'
 };
 
 function handlerBuilder(...funcs) {
@@ -12,11 +15,11 @@ function handlerBuilder(...funcs) {
 }
 
 
-async function handle_follow(jsonBody) {
-  if(jsonBody.eventType !== AIR_EVENT_TYPES.FOLLOW) { return }
+async function handle_follow(event) {
+  if(event.eventType !== AIR_EVENT_TYPES.FOLLOW) { return }
 
-  const lineUserId = jsonBody.lineUserId;
-  const lineDisplayName = jsonBody.lineDisplayName;
+  const lineUserId = event.lineUserId;
+  const lineDisplayName = event.lineDisplayName;
 
   const createMember = () => {
     return new Promise((resolve, reject) => {
@@ -44,7 +47,63 @@ async function handle_follow(jsonBody) {
   return { statusCode: 200, body: 'OK' };
 };
 
+async function handle_reminder(event) {
+  if(event.eventType !== AIR_EVENT_TYPES.REMINDER) { return }
+
+  const idTargets = () => {
+    return new Promise((resolve, reject) => {
+      let table = [];
+      base('LINE-MEMBER')
+        .select({
+          view: "Grid view",
+          cellFormat: "json"
+        })
+        .eachPage(
+          async function page(records, fetchNextPage) {
+            //This function (`page`) will get called for each page of records.
+      
+            records.forEach((record) => {
+              let entry = {
+                  "id": record.id,
+                  "lineUserId": record.fields.LineUserId,
+                  "messageTime": moment(record.fields.MessageTime),
+                  "messageContent": record.fields.MessageContent
+              };
+              table.push(entry);
+            });
+      
+            //To fetch the next page of records, call `fetchNextPage`.
+            //If there are more records, `page` will get called again.
+            //If there are no more records, `done` will get called.
+            await sleep(300);
+            fetchNextPage();
+          },
+          function done(err) {
+            if (err) {
+              console.error(err);
+              return;
+            }
+            //If the scheduled message time is tomorrow
+            let targetEntries = table.filter((entry) => {
+              return moment().add(1, 'days').isSame(entry.messageTime, 'day')
+            });
+  
+            console.log('Retrieved', targetEntries.length, 'records.');
+            resolve(targetEntries);
+          }
+        );
+    });
+  };
+
+  const targets = await idTargets();
+  console.log(targets)
+  //TODO: invoke LINE lambda here
+
+  return { statusCode: 200, body: 'OK' };
+};
+
 
 exports.handler = handlerBuilder(
-  handle_follow
+  handle_follow,
+  handle_reminder
 );

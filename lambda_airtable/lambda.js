c7e209e8481f78446d1fb8e0a4a3b37712c71623
ckccyu <ckccyu@gmail.com>
2019-10-27 13:22:05 +0800

    [Update] generalized retrieval func

--- lambda_airtable/lambda.js
+++ lambda_airtable/lambda.js
@@ -6,7 +6,7 @@ const base = new Airtable({ apiKey: process.env.AIRTABLE_APIKEY }).base(process.
 const AIR_EVENT_TYPES = {
   FOLLOW: 'follow',
   REMINDER: 'reminder',
-  NEXT_CLASS: 'next_class'
+  NEXT_CLASS: 'next_class' //TODO: next class
 };
 
 function handlerBuilder(...funcs) {
@@ -57,53 +57,20 @@ async function handle_follow(event) {
 async function handle_reminder(event) {
   if(event.eventType !== AIR_EVENT_TYPES.REMINDER) { return }
 
-  const idTargets = () => {
-    return new Promise((resolve, reject) => {
-      let table = [];
-      base('LINE-MEMBER')
-        .select({
-          view: "Grid view",
-          cellFormat: "json"
-        })
-        .eachPage(
-          async function page(records, fetchNextPage) {
-            //This function (`page`) will get called for each page of records.
-      
-            records.forEach((record) => {
-              let entry = {
-                  "id": record.id,
-                  "lineUserId": record.fields.LineUserId,
-                  "lineDisplayName": record.fields.LineDisplayName,
-                  "messageTime": moment(record.fields.MessageTime),
-                  "messageContent": record.fields.MessageContent
-              };
-              table.push(entry);
-            });
-      
-            //To fetch the next page of records, call `fetchNextPage`.
-            //If there are more records, `page` will get called again.
-            //If there are no more records, `done` will get called.
-            await sleepPromise(300);
-            fetchNextPage();
-          },
-          function done(err) {
-            if (err) {
-              console.error(err);
-              return;
-            }
-            //If the scheduled message time is tomorrow
-            let targetEntries = table.filter((entry) => {
-              return moment().add(1, 'days').isSame(entry.messageTime, 'day')
-            });
+  const params = {
+    baseName: 'LINE-MEMBER',
+    processRecord: (record) => ({
+      "id": record.id,
+      "lineUserId": record.fields.LineUserId,
+      "lineDisplayName": record.fields.LineDisplayName,
+      "messageTime": moment(record.fields.MessageTime),
+      "messageContent": record.fields.MessageContent    
+    }),
+    //If the scheduled message time is tomorrow
+    filterRecord: (record) => moment().add(1, 'days').isSame(record.messageTime, 'day')
+  }
   
-            console.log('Retrieved', targetEntries.length, 'records.');
-            resolve(targetEntries);
-          }
-        );
-    });
-  };
-
-  const targets = await idTargets();
+  const targets = await retrieve(params);
   return { Status: 'handle_reminder: OK', Data: targets };
 };
 
@@ -112,3 +79,44 @@ exports.handler = handlerBuilder(
   handle_follow,
   handle_reminder
 );
+
+
+//TODO: move the generalization to operation
+function retrieve(params) {
+  let { baseName, processRecord, filterRecord } = params;
+
+  return new Promise((resolve, reject) => {
+    let table = [];
+    base(baseName)
+      .select({
+        view: "Grid view",
+        cellFormat: "json"
+      })
+      .eachPage(
+        async function page(records, fetchNextPage) {
+          //This function (`page`) will get called for each page of records.
+    
+          records.forEach((record) => {
+            let entry = processRecord(record);
+            table.push(entry);
+          });
+    
+          //To fetch the next page of records, call `fetchNextPage`.
+          //If there are more records, `page` will get called again.
+          //If there are no more records, `done` will get called.
+          await sleepPromise(300);
+          fetchNextPage();
+        },
+        function done(err) {
+          if (err) {
+            console.error(err);
+            reject();
+          }
+          
+          let targetEntries = table.filter(filterRecord);
+          console.log('Retrieved', targetEntries.length, 'records.');
+          resolve(targetEntries);
+        }
+      );
+  });
+};

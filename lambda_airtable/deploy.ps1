8bc65a00f156d7cbc9b49415c232ad6f2854d9e3
ckccyu <ckccyu@gmail.com>
2019-11-03 11:57:11 +0800

    [Update] improve deployment

--- lambda_airtable/deploy.ps1
+++ lambda_airtable/deploy.ps1
@@ -1,7 +1,7 @@
 #Path contains build folder and lambda.py file
 $path = "C:\Users\chihy\OneDrive\Ongoing - OneDrive\app-AgingPlus\lambda_airtable\"
 
-Copy-Item $($path + 'lambda.js'), $($path + 'util.js') `
+Copy-Item $($path + '*.js') `
 	-Destination $($path + 'build')
 
 #Use 7z to compress instead of powershell (which sucks) to make the file path compatible in linux systems

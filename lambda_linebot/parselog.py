84d082870a8721e9779396dd305e19f7c155e1e5
ckccyu <ckccyu@gmail.com>
2019-11-18 22:34:29 +0800

    [Add] Parse log into df

--- lambda_linebot/parselog.py
+++ lambda_linebot/parselog.py
@@ -1,6 +1,25 @@
+import os 
 import sys
+venvPath = os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + '\.pyenv\env_linebot\Lib\site-packages'
+sys.path.insert(0, venvPath)
 import json
+import pandas as pd
 
 with open(sys.argv[1]) as f:
-    data = json.load(f)
-    print(data)
+    logs = json.load(f)
+
+records = []
+for log in logs:
+    try:
+        logMsg = json.loads(log['message'])
+        if logMsg.get('logType') == 'MessageEvent':
+            records.append({
+                'timestamp': log['timestamp'],
+                **logMsg
+            })
+
+    except json.JSONDecodeError:
+        continue
+
+df_records = pd.DataFrame.from_records(records)
+print(df_records)

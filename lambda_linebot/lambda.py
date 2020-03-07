1e8225a0e6880a229c9efff6a93d2d755f9f15c5
ckccyu <ckccyu@gmail.com>
2020-03-07 08:52:15 +0800

    [Update] flake conform

--- lambda_linebot/lambda.py
+++ lambda_linebot/lambda.py
@@ -4,14 +4,16 @@ import json
 import re
 from datetime import datetime
 from boto3 import client as boto3_client
-lambda_client = boto3_client('lambda', region_name="us-east-1")
-
 from linebot import WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import FollowEvent, MessageEvent, TextMessage, PostbackEvent
 # https://github.com/line/line-bot-sdk-python
 # https://developers.line.biz/en/reference/messaging-api/
+
+
+# -- Setup
 # TODO: encrypt keys on AWS
+lambda_client = boto3_client('lambda', region_name="us-east-1")
 
 # Get channel_secret from environment variable
 channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
@@ -19,33 +21,60 @@ if channel_secret is None:
     print('Specify LINE_CHANNEL_SECRET as environment variable.')
     sys.exit(1)
 
+# Line webhook
 line_handler = WebhookHandler(channel_secret)
+
+
+# Enum classes
 class AIR_EVENT_TYPES():
     FOLLOW = 'follow'
     REMINDER = 'reminder'
     NEXT_CLASS = 'next_class'
     HOMEWORK = 'homework'
     FINISH_HOMEWORK = 'finish_homework'
+    CLASS_HISTORY = 'class_history'
+    CLASS_RECORD = 'class_record'
     EMPTY = 'empty'
+
+
 class LINE_EVENT_TYPES():
     PUSH = 'push'
     REPLY = 'reply'
     REPLY_CAROUSEL = 'reply_carousel'
+    REPLY_FLEX = 'reply_flex'
     GET_PROFILE = 'get_profile'
+
+
 class LINE_USERACTION_TYPES():
     POSTBACK = 'post_back'
     MESSAGE = 'message'
     URI = 'uri'
+
+
 class LAMBDA():
     AIRTABLE = 'Airtable'
     LINE = 'Line'
 
 
+# Trigger other lambdas (lambda_line, lambda_airtable)
+def invokeLambda(lambdaName, payload):
+    res = lambda_client.invoke(
+        FunctionName=lambdaName,
+        InvocationType='RequestResponse',
+        Payload=json.dumps(payload)
+    )
+
+    # `resPayload` is an array with result of all activated handlers.
+    resPayload = json.loads(res['Payload'].read().decode("utf-8"))
+    return resPayload
+
+
+# -- Main handler
 def lambda_handler(requestEvent, context):
     # Get X-Line-Signature header value
-    signature = requestEvent['headers']['X-Line-Signature']    
-    
-    # Get request body as text    
+    signature = requestEvent['headers']['X-Line-Signature']
+
+    # Get request body as text
     body = requestEvent['body']
 
     # Check the body-signature match and handle the event
@@ -53,29 +82,33 @@ def lambda_handler(requestEvent, context):
         line_handler.handle(body, signature)
     except InvalidSignatureError:
         print("Invalid signature. Please check your channel access token/channel secret.")
-        abort(400)
+        raise
 
     return {'statusCode': 200, 'body': 'OK'}
 
 
-#-- Handle PostbackEvent type
+# -- Handle PostbackEvent type
 @line_handler.add(PostbackEvent)
 def handle_postback(event):
     eventAction = re.search('action=(.+?)(;|$)', event.postback.data)[1]
-    if eventAction == AIR_EVENT_TYPES.EMPTY: pass
+    if eventAction == AIR_EVENT_TYPES.EMPTY:
+        pass
     elif eventAction == AIR_EVENT_TYPES.NEXT_CLASS:
         cmd_nextClass(event)
     elif eventAction == AIR_EVENT_TYPES.HOMEWORK:
         cmd_homework(event)
+    elif eventAction == AIR_EVENT_TYPES.CLASS_HISTORY:
+        cmd_classHistory(event)
     elif eventAction == AIR_EVENT_TYPES.FINISH_HOMEWORK:
-        cmd_finishHomework(event)
+        btn_finishHomework(event)
+
 
 # (User) Reply next class info
 def cmd_nextClass(event):
     '''
-    Success response = 
+    Success response =
     [{'Status': 'handle_nextClass: OK', 'Data': {'memberIid': 'recMgb6f5sfuhVWAs', 'classId': '1900322', 'classTime': '2019-11-14T09:00:00+08:00', 'classLocation': 'home', 'classTrainer': 'CY'}}]
-    '''    
+    '''
     # Get next class info
     resPayload = invokeLambda(LAMBDA.AIRTABLE, {
         'eventType': AIR_EVENT_TYPES.NEXT_CLASS,
@@ -89,8 +122,9 @@ def cmd_nextClass(event):
                 data['classLocation'],
                 data['classTrainer']
             )
-        else: return 'We don\'t have record of your next class 😢.'
-    
+        else:
+            return 'We don\'t have record of your next class 😢.'
+
     # Reply to the message
     invokeLambda(LAMBDA.LINE, {
         'eventType': LINE_EVENT_TYPES.REPLY,
@@ -98,10 +132,11 @@ def cmd_nextClass(event):
         'replyMessage': genReply(resPayload[0]['Data'])
     })
 
+
 # (User) Reply homework info
 def cmd_homework(event):
     '''
-    Success response = 
+    Success response =
     [{'Status': 'handle_homework: OK', 'Data': [{
         hwIid: 'rec123456',
         hwDate: '2020-03-01',
@@ -113,7 +148,7 @@ def cmd_homework(event):
         member: 'Bingo',
         baseMove: '滾筒按摩'
     }]}]
-    '''    
+    '''
     # Get homework info
     resPayload = invokeLambda(LAMBDA.AIRTABLE, {
         'eventType': AIR_EVENT_TYPES.HOMEWORK,
@@ -139,7 +174,7 @@ def cmd_homework(event):
                 'content': {
                     'label': '已完成 ❤️',
                     'data': 'action=empty'
-                }                
+                }
             }] if dataItem['isFinished'] else [{
                 'type': LINE_USERACTION_TYPES.POSTBACK,
                 'content': {
@@ -171,18 +206,118 @@ def cmd_homework(event):
         **genReply(resPayload[0]['Data'])
     })
 
+
 # (User) Update homework info
-def cmd_finishHomework(event):
+def btn_finishHomework(event):
     invokeLambda(LAMBDA.AIRTABLE, {
         'eventType': AIR_EVENT_TYPES.FINISH_HOMEWORK,
         'hwIid': re.search('hwIid=(.+?)(;|$)', event.postback.data)[1]
     })
 
 
-#-- Handle MessageEvent and TextMessage type
+# (User) Reply class history
+def cmd_classHistory(event):
+    '''
+    Success response = 
+
+    '''
+    # TODO: Move template to Line lambda
+    # Get class history
+    # resPayload = invokeLambda(LAMBDA.AIRTABLE, {
+    #     'eventType': AIR_EVENT_TYPES.CLASS_HISTORY,
+    #     'lineUserId': event.source.user_id
+    # })
+
+    def genReplyItem(i, dataItem):
+        return {
+            "type": "bubble",
+            "size": "nano",
+            "header": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                {
+                    "type": "text",
+                    "text": dataItem['date'],
+                    "color": "#ffffff",
+                    "align": "start",
+                    "size": "md",
+                    "gravity": "center"
+                }
+                ],
+                "backgroundColor": "#27ACB2",
+                "paddingTop": "19px",
+                "paddingAll": "12px",
+                "paddingBottom": "16px"
+            },
+            "body": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                {
+                    "type": "text",
+                    "text": dataItem['time'],
+                    "size": "sm",
+                    "align": "start",
+                    "color": "#8C8C8C"
+                },
+                {
+                    "type": "text",
+                    "text": dataItem['location'],
+                    "size": "sm",
+                    "wrap": True,
+                    "color": "#8C8C8C"
+                },
+                {
+                    "type": "text",
+                    "text": dataItem['trainer'],
+                    "size": "sm",
+                    "color": "#8C8C8C"
+                }
+                ],
+                "spacing": "md",
+                "paddingAll": "12px"
+            },
+            "action": {
+                "type": LINE_USERACTION_TYPES.POSTBACK,
+                "label": "action",
+                "data": 'action=class_record;classIid={}'.format(dataItem['classIid']),
+                "displayText": "{} 課程內容".format(dataItem['date'])
+            },
+            "styles": {
+                "footer": {
+                "separator": False
+                }
+            }
+        }
+
+    def genReply(data):
+        if data:  # If the res data is not null
+            reply = [genReplyItem(i, dataItem) for i, dataItem in enumerate(data, 1)]
+
+            # Make dict into json string
+            return {
+                'eventType': LINE_EVENT_TYPES.REPLY_FLEX,
+                'flexType': 'carousel',
+                'replyMessage': json.dumps(reply)
+            }
+        else:
+            return {
+                'eventType': LINE_EVENT_TYPES.REPLY,
+                'replyMessage': 'We don\'t have record of your past classes 😢.'
+            }
+
+    # Reply to the request
+    invokeLambda(LAMBDA.LINE, {
+        'lineReplyToken': event.reply_token,
+        **genReply(resPayload[0]['Data'])
+    })
+
+
+# -- Handle MessageEvent and TextMessage type
 @line_handler.add(MessageEvent, TextMessage)
 def handle_message(event):
-    if event.message.text == 'r': cmd_reminder(event)
+    if event.message.text == 'r': adm_reminder(event)
 
     # TODO: Switch to log module
     print(json.dumps({
@@ -198,15 +333,15 @@ def handle_message(event):
         'replyMessage': event.message.text
     })
 
+
 # 'r' -> (Admin) Send reminder of upcoming classes
-# TODO: Check the admin identity
 # TODO: Deal with no one to send reminder
 # TODO: Admin group message and test
 # TODO: Admin group message by group and indi message
 # TODO: Get everyday log of all message sent to the bot: AWS CLI into csv, time, identity, message
-def cmd_reminder(event):
+def adm_reminder(event):
     '''
-    Success response = 
+    Success response =
     [{'Status': 'handle_reminder: OK', 'Data': [{'iid': 'recGPvFMiUDaoO4', 'lineUserId': 'U9ae6458c650504a3e8380a1046e0f', 'lineDisplayName': 'CY', 'messageTime': '2019-10-28T13:13:00.000Z', 'messageContent': "Hello, this is a response from air."}]}]
     '''
     resPayload = invokeLambda(LAMBDA.AIRTABLE, {
@@ -219,9 +354,9 @@ def cmd_reminder(event):
             'eventType': LINE_EVENT_TYPES.PUSH,
             'lineUserId': target['lineUserId'],
             'pushMessage': target['messageContent']
-        })        
+        })
         remindedInd.append(target['lineDisplayName'])
-    
+
     reply = 'Reminder sent to {}.'.format(', '.join(remindedInd))
     invokeLambda(LAMBDA.LINE, {
         'eventType': LINE_EVENT_TYPES.REPLY,
@@ -230,7 +365,7 @@ def cmd_reminder(event):
     })
 
 
-#-- Handle FollowEvent (when someone adds this account as friend)
+# -- Handle FollowEvent (when someone adds this account as friend)
 @line_handler.add(FollowEvent)
 def handle_follow(event):
     resPayload = invokeLambda(LAMBDA.LINE, {
@@ -253,15 +388,3 @@ def handle_follow(event):
         'lineDisplayName': userDisplayName,
         'lineProfilePic': userProfilePic
     })
-
-
-def invokeLambda(lambdaName, payload):
-    res = lambda_client.invoke(
-        FunctionName=lambdaName,
-        InvocationType='RequestResponse',
-        Payload=json.dumps(payload)
-    )
-
-    # `resPayload` is an array with result of all activated handlers.
-    resPayload = json.loads(res['Payload'].read().decode("utf-8"))
-    return resPayload

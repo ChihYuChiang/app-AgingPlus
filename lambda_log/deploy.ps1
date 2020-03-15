9a9c9a508107f37611916e939e6d7d2657df61dd
ckccyu <ckccyu@gmail.com>
2020-03-15 14:08:12 +0800

    [Update] Improve node package build

--- lambda_log/deploy.ps1
+++ lambda_log/deploy.ps1
@@ -1,14 +1,28 @@
-# Path contains build folder and lambda.py file
+# Path contains build folder and source files
 $path = "C:\Users\chihy\OneDrive\Ongoing - OneDrive\app-AgingPlus\lambda_log\"
 
-Copy-Item $($path + '*.js') `
-	-Destination $($path + 'build')
 
+# Move content to build folder and install prod dependency
+Copy-Item `
+	$($path + '*.js'), `
+	$($path + 'package.json'), `
+	$($path + 'package-lock.json') `
+	-Destination $($path + 'build')
+Set-Location $($path + 'build\')
+npm install --only=prod
+
+
+# Delete old build
+Remove-Item –path $($path + 'Log.zip')
+
+
+# Make new build
 # Use 7z to compress instead of powershell (which sucks) to make the file path compatible in linux systems
 Set-Location "C:\Program Files\7-Zip\"
-.\7z u $($path + 'Log.zip') `
-	$($path + 'build\*') $($path + 'node_modules')
+.\7z a $($path + 'Log.zip')	$($path + 'build\*')
 
+
+# Update Lambda
 aws lambda update-function-code `
     --function-name Log `
     --zip-file $("fileb://" + $path + "Log.zip") `

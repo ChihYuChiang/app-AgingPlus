5757bacd0f774563c49e9768021dcfe9bec22f80
ckccyu <ckccyu@gmail.com>
2020-03-15 11:21:36 +0800

    [Base] Taming wild export

--- lambda_log/deploy.ps1
+++ lambda_log/deploy.ps1
@@ -1,14 +1,15 @@
-#Path contains build folder and lambda.py file
+# Path contains build folder and lambda.py file
 $path = "C:\Users\chihy\OneDrive\Ongoing - OneDrive\app-AgingPlus\lambda_log\"
 
-Copy-Item $($path + 'lambda.py') `
+Copy-Item $($path + '*.js') `
 	-Destination $($path + 'build')
 
+# Use 7z to compress instead of powershell (which sucks) to make the file path compatible in linux systems
 Set-Location "C:\Program Files\7-Zip\"
-.\7z u $($path + 'LineBot.zip') `
-	$($path + 'build\*')
+.\7z u $($path + 'Log.zip') `
+	$($path + 'build\*') $($path + 'node_modules')
 
 aws lambda update-function-code `
     --function-name Log `
-    --zip-file $("fileb://" + $path + "LineBot.zip") `
+    --zip-file $("fileb://" + $path + "Log.zip") `
 	| Select-String -Pattern '(FunctionName)|(LastModified)'
--- lambda_log/lambda.py
+++
@@ -1,2 +0,0 @@
-from boto3 import client as boto3_client
-lambda_client = boto3_client('lambda', region_name="us-east-1")

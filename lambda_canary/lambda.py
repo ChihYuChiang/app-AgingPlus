d65b4feaa0434830c34afe527ac52408f37c6910
ckccyu <ckccyu@gmail.com>
2020-03-21 17:10:01 +0800

    [Update] Style comformation

--- lambda_canary/lambda.py
+++ lambda_canary/lambda.py
@@ -3,17 +3,23 @@ from datetime import datetime, date
 from boto3 import client as boto3_client
 lambda_client = boto3_client('lambda', region_name="us-east-1")
 
+
 class AIR_EVENT_TYPES():
     REMINDER = 'reminder'
+
+
 class LINE_EVENT_TYPES():
     PUSH = 'push'
+
+
 class LAMBDA():
     AIRTABLE = 'Airtable'
     LINE = 'Line'
 
+
 class Scheduler():
     scheduledJobs = []
-    
+
     @classmethod
     def add(cls, evalExp):
         def wrapper(func):
@@ -25,10 +31,11 @@ def lambda_handler(requestEvent, context):
     print('Checking {} at {}...'.format('scheduled jobs', requestEvent['time']))
 
     res = [event() for event in Scheduler.scheduledJobs]
-    
+
     print('Check complete at {}.'.format(str(datetime.now())))
 
-@Scheduler.add('not date.today().day % 1') #This execute everyday
+
+@Scheduler.add('not date.today().day % 1')  # This execute everyday
 def job_reminder():
     resPayload = invokeLambda(LAMBDA.AIRTABLE, {
         'eventType': AIR_EVENT_TYPES.REMINDER
@@ -53,6 +60,6 @@ def invokeLambda(lambdaName, payload):
         Payload=json.dumps(payload)
     )
 
-    #`resPayload` is an array with result of all activated handlers.
+    # `resPayload` is an array with result of all activated handlers.
     resPayload = json.loads(res['Payload'].read().decode("utf-8"))
     return resPayload
